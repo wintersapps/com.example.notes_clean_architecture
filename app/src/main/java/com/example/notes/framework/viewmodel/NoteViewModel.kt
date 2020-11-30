d0be37f2a0dcc6a11e10d3c6bdce10f476d1df26
Patryk Winter <patrykwintersapps@gmail.com>
2020-11-30 11:15:20 +0100

    MVVM architecture added

---
+++ app/src/main/java/com/example/notes/framework/viewmodel/NoteViewModel.kt
@@ -0,0 +1,38 @@
+package com.example.notes.framework.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.example.core.data.Note
+import com.example.core.repository.NoteRepository
+import com.example.core.usecase.AddNote
+import com.example.core.usecase.GetAllNotes
+import com.example.core.usecase.GetNote
+import com.example.core.usecase.RemoveNote
+import com.example.notes.framework.RoomNoteDataSource
+import com.example.notes.framework.UseCases
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class NoteViewModel(application: Application): AndroidViewModel(application) {
+    private val coroutineScope = CoroutineScope(Dispatchers.IO)
+
+    private val repository = NoteRepository(RoomNoteDataSource(application))
+
+    private val useCases = UseCases(
+        AddNote(repository),
+        GetAllNotes(repository),
+        GetNote(repository),
+        RemoveNote(repository)
+    )
+
+    val saved by lazy { MutableLiveData<Boolean>() }
+
+    fun saveNote(note: Note){
+        coroutineScope.launch {
+            useCases.addNote(note)
+            saved.postValue(true)
+        }
+    }
+}
--- app/src/main/java/com/example/notes/presentation/fragments/NoteFragment.kt
+++ app/src/main/java/com/example/notes/presentation/fragments/NoteFragment.kt
@@ -1,18 +1,28 @@
 package com.example.notes.presentation.fragments
 
+import android.content.Context.INPUT_METHOD_SERVICE
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
+import com.example.core.data.Note
 import com.example.notes.R
 import com.example.notes.databinding.FragmentNoteBinding
+import com.example.notes.framework.viewmodel.NoteViewModel
 
 class NoteFragment : Fragment() {
 
     private var _binding: FragmentNoteBinding? = null
     private val binding get() = _binding!!
+    private val viewModel: NoteViewModel by viewModels()
+    private var currentNote = Note("", "", 0L, 0L)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,8 +34,8 @@ class NoteFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         setListeners()
+        observeViewModel()
     }
 
     override fun onDestroyView() {
@@ -34,8 +44,78 @@ class NoteFragment : Fragment() {
     }
 
     private fun setListeners() {
+
+        binding.titleEditText.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun afterTextChanged(p0: Editable?) {
+                if(binding.titleEditText.text.isNullOrEmpty()){
+                    binding.titleTextLayout.error = getString(R.string.this_field_can_not_be_empty)
+                }else{
+                    binding.titleTextLayout.error = null
+                }
+            }
+        })
+
+        binding.contentEditText.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun afterTextChanged(p0: Editable?) {
+                if(binding.contentEditText.text.isNullOrEmpty()){
+                    binding.contentTextLayout.error = getString(R.string.this_field_can_not_be_empty)
+                }else{
+                    binding.contentTextLayout.error = null
+                }
+            }
+        })
+
         binding.saveNoteFab.setOnClickListener {
-            Navigation.findNavController(it).popBackStack()
+            if(areFieldsValid()){
+                val time = System.currentTimeMillis()
+                currentNote.title = binding.titleEditText.text.toString()
+                currentNote.content = binding.contentEditText.text.toString()
+                currentNote.updateTime = time
+                if(currentNote.id == 0L){
+                    currentNote.creationTime = time
+                }
+                viewModel.saveNote(currentNote)
+            }else{
+                Toast.makeText(context, getString(R.string.correct_the_fields_with_errors), Toast.LENGTH_SHORT).show()
+            }
         }
+    }
+
+    private fun observeViewModel(){
+        viewModel.saved.observe(viewLifecycleOwner, { saved ->
+            saved?.let {
+                if(it){
+                    Toast.makeText(context, getString(R.string.done), Toast.LENGTH_SHORT).show()
+                    hideKeyboard()
+                    Navigation.findNavController(binding.saveNoteFab).popBackStack()
+                }else{
+                    Toast.makeText(context, getString(R.string.something_has_gone_wrong_please_try_again), Toast.LENGTH_SHORT).show()
+                }
+            }
+        })
+    }
+
+    private fun areFieldsValid(): Boolean{
+        var areValid = true
+
+        if(binding.titleEditText.text.isNullOrEmpty()){
+            binding.titleTextLayout.error = getString(R.string.this_field_can_not_be_empty)
+            areValid = false
+        }
+        if (binding.contentEditText.text.isNullOrEmpty()){
+            binding.contentTextLayout.error = getString(R.string.this_field_can_not_be_empty)
+            areValid = false
+        }
+
+        return areValid
+    }
+
+    private fun hideKeyboard(){
+        val inputMethodManager = binding.root.context.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(binding.root.windowToken, 0)
     }
 }
